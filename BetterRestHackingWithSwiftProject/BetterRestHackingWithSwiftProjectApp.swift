a7af4bcdf9c81a05f7bec15cfad4a38efecc5cdd
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2025-01-12 12:53:44 +0530

    Initial Commit

---
+++ BetterRestHackingWithSwiftProject/BetterRestHackingWithSwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestHackingWithSwiftProjectApp.swift
+//  BetterRestHackingWithSwiftProject
+//
+//  Created by Shubham on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestHackingWithSwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
